8a333026072fed6482adecf570372acce6e864e1
tetsulow <tetsu.miyawaki@gmail.com>
2022-05-26 09:08:36 +0000

    Update Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
 
-  # before_action :require_sign_in
-    # before_action :current_user
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
 
-    def current_user?(user)
-      user == current_user
-    end
+    # def current_user?(user)
+    #   user == current_user
+    # end
 
     def require_sign_in
         redirect_to root_path unless current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,5 @@
 class SessionsController < ApplicationController
-
-#   skip_before_action :require_sign_in, only: [:login]
-#   skip_before_action :current_user, only: [:login]
+    
     def login
         if params[:session]
             @user = User.find_by(name: params[:session][:name], password: params[:session][:password])
@@ -9,7 +7,6 @@ class SessionsController < ApplicationController
                 session[:user_id] = @user.id
                 flash[:notice] = "ログインしました"
                 redirect_to root_path
-                # user_path(session[:user_id])
             else
                 render("sessions/login")
             end
