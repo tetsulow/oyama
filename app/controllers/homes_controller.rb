62eb7a3f4ecbd5cc45a09ad758fd91f43de17092
tetsulow <tetsu.miyawaki@gmail.com>
2022-05-27 00:44:14 +0000

    Add intro

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,8 @@ class HomesController < ApplicationController
 
   def top
     @list = List.new
-    @lists = params[:name].present? ? Tag.find(params[:name]).lists : List.all
-    @tags = Tag.all
+    @lists = params[:name].present? ? Tag.find(params[:name]).lists : List.all  #nameがセットされていたらTagから関連づけられたlistsを呼び、nameの指定がなければ、全ての投稿を表示する
+    @tags = Tag.all  #全ての登録されたタグを取得
   end
 
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,26 +2,26 @@ class ListsController < ApplicationController
 
   def new
     @list = List.new
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])   #管理人にのみ操作できる機能を付与
   end
 
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list.id)
+      redirect_to list_path(@list.id)  #新しい記事が作成できたら記事の詳細ページに移行
     else
-      render :new
+      render :new  #記事の作成に失敗したら記事作成画面に戻る  
     end
   end
 
   def index
-    @lists = List.all
+    @lists = List.all  #投稿された全て記事を取得
   end
 
   def show
     @list = List.find(params[:id])
     @list_comment = ListComment.new
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])   #管理人にのみ操作できる機能を付与
   end
 
   def edit
@@ -31,7 +31,7 @@ class ListsController < ApplicationController
   def update
     @list = List.find(params[:id])
     if @list.update(list_params)
-      redirect_to list_path(@list)
+      redirect_to list_path(@list)    #編集した記事が保存できたら記事の詳細ページに
     else
       render :edit
     end
